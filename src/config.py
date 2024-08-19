99b0efb93d030c243fa2e65c362674728d4c8b79
Shradaya <shradayaramtel@lftechnology.com>
2024-08-19 12:52:09 +0545

    model name addition

--- src/config.py
+++ src/config.py
@@ -2,7 +2,10 @@ class app:
     title = "Test Application"
 
 class ollama_configs:
-    model_name = "llama3"
+    model_name = "qwen:0.5b"
+    # model_name = "qwen2:0.5b"
+    # model_name = "starcoder:1b"
+    # model_name = "llama3.1"
     
 class file_paths:
     new = "./pdfs/"
@@ -12,7 +15,7 @@ class qdrant_configs:
     HOST = "localhost"
     PORT = "6333"
     URI = f"http://{HOST}:{PORT}/"
-    COLLECTION = "tft"
+    COLLECTION = f"constitution-{ollama_configs.model_name.replace('.', '-').replace(':', '-')}"
     
 CUSTOM_PROMPT = """
 You are an AI assistant specialized in the Critical perspective in accounting.
